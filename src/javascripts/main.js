cc00ea30545570738cd4c7dd8465c8dc19797e8a
Laura Collins <laura.e.collins1@gmail.com>
2019-10-02 19:55:29 -0500

    setup complete

---
+++ src/javascripts/main.js
@@ -0,0 +1,10 @@
+import '../styles/main.scss';
+import '@fortawesome/fontawesome-free/js/all';
+
+const init = () => {
+  document.getElementById('pet').innerHTML = '<i class="fas fa-heart"></i>';
+
+  console.log('hi');
+};
+
+init();
