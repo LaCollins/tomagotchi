a4d514e409235e6f1a5ba078fc3777f3ff0f6462
Laura Collins <laura.e.collins1@gmail.com>
2019-10-05 14:17:01 -0500

    play module done

---
+++ src/javascripts/helpers/data/playData.js
@@ -0,0 +1,21 @@
+const play = [
+  {
+    type: 'video games',
+    score: 50,
+    img: '<i class="fas fa-gamepad"></i>',
+  },
+  {
+    type: 'football',
+    score: 10,
+    img: '<i class="fas fa-football-ball"></i>',
+  },
+  {
+    type: 'chess',
+    score: 2,
+    img: '<i class="fas fa-chess-knight"></i>',
+  },
+];
+
+const getPlayData = () => play;
+
+export default { getPlayData };
