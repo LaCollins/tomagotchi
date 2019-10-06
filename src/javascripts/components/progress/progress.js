fd563db4188e61425f7cbae22dd6721008c7a0ef
Laura Collins <laura.e.collins1@gmail.com>
2019-10-06 12:51:26 -0500

    wip

--- src/javascripts/components/progress/progress.js
+++ src/javascripts/components/progress/progress.js
@@ -3,28 +3,30 @@ import fight from '../fight/fight';
 import play from '../play/play';
 import sleep from '../sleep/sleep';
 import utilities from '../../helpers/utilities';
+import './progress.scss';
 
 const happinessBar = (progress) => {
   const progressString = `
         <div class="happinessMeter">
         <span style="width: ${progress}%" id="bar"></span>
-        <div class="progressPercentage">${progress}%</div>
-        <h2>Happiness</h2>
-        </div>
+        <div class="progressPercentage">Happiness ${progress}%</div>
         </div>
         `;
   utilities.printToDom('progress', progressString);
 };
 
+
 const findHappiness = () => {
-  const full = eat.getFullProgress();
-  const strength = fight.getFightProgress();
-  const fun = play.getPlayProgress();
-  const energy = sleep.getSleepProgress();
-
-  const totalHappiness = (full + strength + fun + energy) / 4;
-
+  let totalHappiness = 75;
   happinessBar(totalHappiness);
+  setInterval(() => {
+    const full = eat.getFullProgress();
+    const strength = fight.getFightProgress();
+    const energy = sleep.getSleepProgress();
+    const fun = play.getPlayProgress();
+    totalHappiness = Math.round((fun + full + strength + energy) / 4);
+    happinessBar(totalHappiness);
+  }, 5000);
 };
 
 export default { findHappiness };
